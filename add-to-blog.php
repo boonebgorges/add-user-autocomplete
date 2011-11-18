f8e15441ff19d4552751dbec9bb528686fe2d39b
Boone Gorges <boonebgorges@gmail.com>
2011-11-18 14:15:59 -0500

    Moves to regular Add New User page

--- add-to-blog.php
+++ add-to-blog.php
@@ -14,16 +14,12 @@ class Add_To_Blog {
 	var $base_url;
 	
 	function __construct() {
-		add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
-		$this->base_url = add_query_arg( 'page', 'add-to-blog', admin_url( 'users.php' ) );
-		
+		add_action( 'admin_print_styles', array( $this, 'add_admin_styles' ) );
+		add_action( 'admin_print_scripts', array( $this, 'add_admin_scripts' ) );
 		add_action( 'wp_ajax_add_to_blog_find_user', array( &$this, 'autocomplete_results' ) );
 	}
 	
 	function admin_menu() {
-		$page = add_users_page( __( 'Add To Blog', 'a2b' ), __( 'Add To Blog', 'a2b' ), 'create_users', 'add-to-blog', array( $this, 'admin_panel_main' ) );
-		add_action( "admin_print_styles-$page", array( $this, 'add_admin_styles' ) );
-		add_action( "admin_print_scripts-$page", array( $this, 'add_admin_scripts' ) );
 
 	}
 	
@@ -46,7 +42,7 @@ class Add_To_Blog {
 		);
 	
 		$users = new WP_User_Query( array(
-			'blog_id' => $_REQUEST['blog_id'],
+			'blog_id' => get_current_blog_id(),
 			'search'  => '*' . $_REQUEST['query'] . '*'
 		) );
 		
@@ -65,29 +61,6 @@ class Add_To_Blog {
 	
 		echo json_encode( $return );
 		die();
-	}
-	
-	function admin_panel_main() {
-		?>
-		
-		<div class="wrap">
-		<form action="<?php echo $this->base_url ?>" method="post">
-			
-			<div id="icon-users" class="icon32">
-			<br>
-			</div>
-			<h2><?php _e( 'Add To Blog', 'a2b' ) ?></h2>
-			
-			<br />
-			
-			<input id="adduser-email" type="text" name="email" value="" />
-			<input id="adduser-blog-id" type="hidden" value="<?php echo get_current_blog_id() ?>" />
-			
-		
-			
-		</form>
-		</div>
-		<?php
 	}
 }
 
--- js/add-to-blog.js
+++ js/add-to-blog.js
@@ -1,18 +1,40 @@
 jQuery(document).ready(function($) {
+	var ainput = $('#adduser-email');
+	
+	$(ainput).after('<ul id="add-to-blog-users"></ul>');
+	$(ainput).bind('keyup',function(event){
+		if( ( 45 < event.keyCode && event.keyCode < 91 ) || event.keyCode == 8 ) {
+			$(ainput).addClass('loading');
+		}
+	});
+	
+	var ulist = $('#add-to-blog-users');
+	
 	var options = {
 		serviceUrl: ajaxurl,
+		fnFormatResult: a2bFormatResult,
 		width: 300,
 		delimiter: /(,|;)\s*/,
-		onSelect: function(){alert('tr');},
-		deferRequestBy: 0, //miliseconds
+		onSelect: function(dname,user_id){ btest(dname,user_id); },
+		deferRequestBy: 500, // miliseconds
 		params: { 
-			action: 'add_to_blog_find_user', 
-			blog_id: $('#adduser-blog-id').val() 
+			action: 'add_to_blog_find_user'
 		},
 		noCache: true //set to true, to disable caching
 	};
 	
-	a = $('#adduser-email').autocomplete(options);
+	a = $(ainput).autocomplete(options);
 
+	function btest(dname,user_id){
+		$(ulist).append('<li class="atb-user" id="atb-user-' + user_id + '">' + dname + '</li>');
+	}
+	
+	function a2bFormatResult(value, data, currentValue) {
+		$(ainput).removeClass('loading');
+		
+		var a2breEscape = new RegExp('(\\' + ['/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\'].join('|\\') + ')', 'g');
+		var a2bpattern = '(' + currentValue.replace(a2breEscape, '\\$1') + ')';
+		return value.replace(new RegExp(a2bpattern, 'gi'), '<strong>$1<\/strong>');
+	}
 	
 },(jQuery));
